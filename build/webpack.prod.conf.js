026176a9434f98cad21473186f8a38ab906eca7f
syumai <syumai@gmail.com>
2017-03-07 15:27:12 +0900

    Removed minifier

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -48,13 +48,6 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'src/index.html',
       inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
